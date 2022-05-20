9de08a75c048ac1871ed246f6841add3eb6bf682
Grihit <grihitbudhiraja12@gmail.com>
2022-05-20 16:30:57 +0530

    Deployed using firebase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,29 @@
 import React from "react";
-import {
-    Box,
-    Flex, 
-} from '@chakra-ui/react'
-import {
-    Link as RouterLink, 
-} from "react-router-dom"
-
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <Box>
-        <Flex 
-            bg={'gray.50'} 
-            w={'full'} 
-            minH={'80px'}
-            alignItems={'center'}
-            boxShadow={'0px 15px 34px -19px rgba(57,192,186,0.3)'}
-            paddingLeft={'20px'}
-        >
-            <RouterLink to="/">
-                <img src="https://www.disecto.com/static/media/logo.244765c6.svg" alt="Disecto logo" /> 
-            </RouterLink>
-        </Flex>
+      <Flex
+        bg={"gray.50"}
+        w={"full"}
+        minH={"80px"}
+        alignItems={"center"}
+        boxShadow={"0px 15px 34px -19px rgba(57,192,186,0.3)"}
+        paddingLeft={"20px"}
+      >
+        <RouterLink to="/">
+          <Text
+            fontFamily={"Inter, sans-serif"}
+            fontWeight={"700"}
+            fontSize={"3xl"}
+            color={"#39c0ba"}
+          >
+            Image Gallery
+          </Text>
+        </RouterLink>
+      </Flex>
     </Box>
   );
 }
